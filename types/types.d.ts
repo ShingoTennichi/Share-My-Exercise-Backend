8b20aa3d2967da117ac7fd98cf8b4f7234d3f1a0
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-07-08 22:35:37 -0700

    added a dependency

---
+++ types/types.d.ts
@@ -0,0 +1,47 @@
+export type SignUpInfo = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  deletedAt: Date?;
+};
+
+export type SignInInfo = {
+  email: string,
+  password: string
+}
+
+export type Follow = {
+  followeeId: number;
+  followingId: number;
+  status: boolean;
+  deletedAt: Date?;
+};
+
+export type Post = {
+  authorId: number;
+  imgUrl: string;
+  like: number;
+  text: string;
+  deletedAt: Date?;
+};
+
+export type Comment = {
+  authorId: number;
+  postId: number;
+  text: string;
+  deletedAt: Date?;
+};
+
+export type UserActivity = {
+  authorId: number;
+  activityId: number;
+  startedAt: Date;
+  finishedAt: Date;
+  breakInMin: number;
+  deletedAt: Date?;
+};
+
+export type Activity = {
+  activityName: string;
+};
