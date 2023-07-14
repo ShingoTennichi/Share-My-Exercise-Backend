7ab47e34def971a289c5de3b7b773b768ca0dd04
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-07-14 10:53:50 -0700

    added user-activity route

---
+++ app/api/activity/user-activity/route.ts
@@ -0,0 +1,36 @@
+import { globalPrisma } from "@/prisma/globalPrismaClient";
+
+export async function POST(response: Response, request: Request) {
+  try {
+    // get request body
+    const { id } = await response.json();
+
+    const prisma: globalPrisma = globalPrisma;
+    // get al activities matched to the user id requested
+    const data = await prisma.userActivity.findMany({
+      where: {
+        authorId: id
+      }
+    });
+
+    return new Response(
+      JSON.stringify({
+        data: {
+          status: "Success",
+          message: "Successfully processed",
+          result: data,
+        },
+      })
+    );
+  }catch(e) {
+    return new Response(
+      JSON.stringify({
+        data: {
+          status: "Error",
+          message: `${e}`,
+          result: {},
+        },
+      })
+    );
+  }
+}
