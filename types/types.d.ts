4610ada8a869991cbf27fd949c819e011a603efe
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-07-14 10:52:34 -0700

    added and romoced some types

--- types/types.d.ts
+++ types/types.d.ts
@@ -3,7 +3,6 @@ export type SignUpInfo = {
   lastName: string;
   email: string;
   password: string;
-  deletedAt: Date?;
 };
 
 export type SignInInfo = {
@@ -11,42 +10,16 @@ export type SignInInfo = {
   password: string;
 };
 
-export type Follow = {
-  followeeId: number;
-  followingId: number;
-  status: boolean;
-  deletedAt: Date?;
-};
-
-export type Post = {
-  authorId: number;
-  imgUrl: string;
-  like: number;
-  text: string;
-  deletedAt: Date?;
-};
-
-export type Comment = {
-  authorId: number;
-  postId: number;
-  text: string;
-  deletedAt: Date?;
-};
-
-export type UserActivity = {
-  authorId: number;
-  activityId: number;
-  startedAt: Date;
-  finishedAt: Date;
-  breakInMin: number;
-  deletedAt: Date?;
-};
-
 export type Activity = {
   authorId: number;
   activityId: number;
-  name: string;
   startedAt: Date;
   finishedAt: Date;
   duration: number;
+};
+
+export type NewPost = {
+  authorId: number;
+  imgUrl: string;
+  text: string;
 };
