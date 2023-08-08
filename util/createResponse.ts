2f74ce960a767c8a125cf3cb94265871aaecf720
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-08-07 17:26:39 -0700

    add a method for api response

---
+++ util/createResponse.ts
@@ -0,0 +1,23 @@
+type Status = "success" | "error";
+
+export function createResponse(status: Status, data: {} = {}): Response {
+  if(status === "success") {
+    return new Response(
+      JSON.stringify({
+        data: {
+          status: "success",
+          result: data,
+        },
+      })
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        data: {
+          status: "error",
+          result: data,
+        },
+      })
+    );
+  }
+}
